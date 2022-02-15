90b2f0e6fa13975550ca63a9196d059063c61f25
OWNER <OWNER@DESKTOP-ICPAMTI>
2022-02-15 08:24:50 +0530

    By Tushar

--- GitDemoTB13/src/Demo.java
+++ GitDemoTB13/src/Demo.java
@@ -1,6 +1,11 @@
 
 public class Demo {
 
+	public void m1() {
+		System.out.println("This is M1 Of Demo - GitDemoTB13");
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("This is main method of Demo - GitDemoTB13.");
 	}
